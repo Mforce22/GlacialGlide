c4913e09cd329a62cf470dc5cacfa936b8b929d0
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-07-13 12:35:44 +0200

    Modified spawn logic to work with PoolingSystem

--- Assets/Scripts/Utils/SpawnCollectibles.cs
+++ Assets/Scripts/Utils/SpawnCollectibles.cs
@@ -15,41 +15,41 @@ public class SpawnCollectibles : MonoBehaviour
     [Tooltip("Trees spawn rate")]
     private int Trees_SpawnRate;
     [SerializeField]
-    private GameObject Tree1Prefab;
+    private IdContainer Tree1ManagerContainer;
     [SerializeField]
-    private GameObject Tree2Prefab;
+    private IdContainer Tree2ManagerContainer;
     [SerializeField]
-    private GameObject Tree3Prefab;
+    private IdContainer Tree3ManagerContainer;
 
     [SerializeField]
     [Tooltip("Bombs spawn rate")]
     private int Bombs_SpawnRate;
     [SerializeField]
-    private GameObject BombPrefab;
+    private IdContainer BombManagerContainer;
 
     [SerializeField]
     [Tooltip("Coin spawn rate")]
     private int Coin_SpawnRate;
     [SerializeField]
-    private GameObject CoinPrefab;
+    private IdContainer CoinManagerContainer;
 
     [SerializeField]
     [Tooltip("Shield spawn rate")]
     private int Shield_SpawnRate;
     [SerializeField]
-    private GameObject ShieldPrefab;
+    private IdContainer ShieldManagerContainer;
 
     [SerializeField]
     [Tooltip("X2 spawn rate")]
     private int X2_SpawnRate;
     [SerializeField]
-    private GameObject X2Prefab;
+    private IdContainer X2ManagerContainer;
 
     [SerializeField]
     [Tooltip("Heart spawn rate")]
     private int Heart_SpawnRate;
     [SerializeField]
-    private GameObject HeartPrefab;
+    private IdContainer HeartManagerContainer;
 
     [SerializeField]
     [Tooltip("Ramp spawn rate")]
@@ -64,11 +64,11 @@ public class SpawnCollectibles : MonoBehaviour
     [Tooltip("Big ramp spawn rate")]
     private int LRamp_SpawnRate;
     [SerializeField]
-    private GameObject SRampPrefab;
+    private IdContainer SRampManagerContainer;
     [SerializeField]
-    private GameObject MRampPrefab;
+    private IdContainer MRampManagerContainer;
     [SerializeField]
-    private GameObject LRampPrefab;
+    private IdContainer LRampManagerContainer;
 
     [SerializeField]
     private GameObject SpawnPoint;
@@ -79,96 +79,120 @@ public class SpawnCollectibles : MonoBehaviour
     }
 
     private IEnumerator SpawnCollectiblesCoroutine() {
-
         while (true) {
             //move the spawn point
             SpawnPoint.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), SpawnPoint.transform.position.y, SpawnPoint.transform.position.z);
 
             //wait 0.6 seconds
             yield return new WaitForSeconds(SpawnRate);
-            int randomValue = Random.Range(0, 100);
 
-            //60 spawn rate trees
-            if(randomValue < Trees_SpawnRate) {
-                SpawnTree();
-            }
-            //20 spawn rate coins
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate) && randomValue > Trees_SpawnRate) {
-                SpawnCoin();
-            }
-            //6 spawn rate bombs
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate)) {
-                SpawnBomb();
-            }
-            //6 spawn rate ramps
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate)) {
-                SpawnRamp();
-            }
-            //4 spawn rate shield
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate)) {
-                SpawnShield();
-            }
-            //2 spawn rate X2
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate)) {
-                SpawnX2();
-            }
-            //2 spawn rate Heart
-            else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate + Heart_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate)) {
-                SpawnHeart();
+            if (!GameMaster.Instance.getPause()) {
+                int randomValue = Random.Range(0, 100);
+
+                //60 spawn rate trees
+                if (randomValue < Trees_SpawnRate) {
+                    SpawnTree();
+                }
+                //20 spawn rate coins
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate) && randomValue > Trees_SpawnRate) {
+                    SpawnCoin();
+                }
+                //6 spawn rate bombs
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate)) {
+                    SpawnBomb();
+                }
+                //6 spawn rate ramps
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate)) {
+                    SpawnRamp();
+                }
+                //4 spawn rate shield
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate)) {
+                    SpawnShield();
+                }
+                //2 spawn rate X2
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate)) {
+                    SpawnX2();
+                }
+                //2 spawn rate Heart
+                else if (randomValue < (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate + Heart_SpawnRate) && randomValue > (Trees_SpawnRate + Coin_SpawnRate + Bombs_SpawnRate + Ramp_SpawnRate + Shield_SpawnRate + X2_SpawnRate)) {
+                    SpawnHeart();
+                }
             }
         }
     }
 
     private void SpawnTree() {
         int randomIndex = Random.Range(1, 4);
-        GameObject treePrefab = null;
-
+        PoolableObject treePrefab = null;
+        PoolManager poolManager = null;
+        
         switch (randomIndex) {
             case 1:
-                treePrefab = Tree1Prefab;
+                poolManager = PoolingSystem.Instance.getPoolManagerInstance(Tree1ManagerContainer);
+                treePrefab = poolManager.GetPoolableObject<PoolableObject>();
                 break;
             case 2:
-                treePrefab = Tree2Prefab;
+                poolManager = PoolingSystem.Instance.getPoolManagerInstance(Tree2ManagerContainer);
+                treePrefab = poolManager.GetPoolableObject<PoolableObject>();
                 break;
             case 3:
-                treePrefab = Tree3Prefab;
+                poolManager = PoolingSystem.Instance.getPoolManagerInstance(Tree3ManagerContainer);
+                treePrefab = poolManager.GetPoolableObject<PoolableObject>();
                 break;
         }
 
         if (treePrefab != null) {
-            Instantiate(treePrefab, SpawnPoint.transform.position, Quaternion.identity);
+            treePrefab.transform.position = SpawnPoint.transform.position;
         }
     }
     private void SpawnBomb() {
-        Instantiate(BombPrefab, SpawnPoint.transform.position, Quaternion.identity);
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(BombManagerContainer);
+        PoolableObject prefab = poolManager.GetPoolableObject<PoolableObject>();
+        prefab.transform.position = SpawnPoint.transform.position;
     }
     private void SpawnCoin() {
-        Instantiate(CoinPrefab, SpawnPoint.transform.position, Quaternion.identity);
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(CoinManagerContainer);
+        PoolableObject prefab = poolManager.GetPoolableObject<PoolableObject>();
+        prefab.transform.position = SpawnPoint.transform.position;
     }
     private void SpawnShield() {
-        Instantiate(ShieldPrefab, SpawnPoint.transform.position, Quaternion.identity);
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(ShieldManagerContainer);
+        PoolableObject prefab = poolManager.GetPoolableObject<PoolableObject>();
+        prefab.transform.position = SpawnPoint.transform.position;
     }
     private void SpawnX2() {
-        Instantiate(X2Prefab, SpawnPoint.transform.position, Quaternion.identity);
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(X2ManagerContainer);
+        PoolableObject prefab = poolManager.GetPoolableObject<PoolableObject>();
+        prefab.transform.position = SpawnPoint.transform.position;
     }
     private void SpawnHeart() {
-        Instantiate(HeartPrefab, SpawnPoint.transform.position, Quaternion.identity);
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(HeartManagerContainer);
+        PoolableObject prefab = poolManager.GetPoolableObject<PoolableObject>();
+        prefab.transform.position = SpawnPoint.transform.position;
     }
     private void SpawnRamp() {
-        GameObject rampPrefab = null;
+        int randomValue = Random.Range(0, 100);
+        PoolableObject rampPrefab = null;
+        PoolManager poolManager = null;
 
-        if (Random.Range(0, 100) < SRamp_SpawnRate) {
-            rampPrefab = SRampPrefab;
+        //75 spawn rate small ramp
+        if (randomValue < SRamp_SpawnRate) {
+            poolManager = PoolingSystem.Instance.getPoolManagerInstance(SRampManagerContainer);
+            rampPrefab = poolManager.GetPoolableObject<PoolableObject>();
         }
-        if (Random.Range(0, 100) < MRamp_SpawnRate) {
-            rampPrefab = MRampPrefab;
+        //20 spawn rate medium ramp
+        else if (randomValue < (SRamp_SpawnRate + MRamp_SpawnRate) && randomValue > SRamp_SpawnRate) {
+            poolManager = PoolingSystem.Instance.getPoolManagerInstance(MRampManagerContainer);
+            rampPrefab = poolManager.GetPoolableObject<PoolableObject>();
         }
-        if (Random.Range(0, 100) < LRamp_SpawnRate) {
-            rampPrefab = MRampPrefab;
+        //5 spawn rate big ramp
+        else if (randomValue < (SRamp_SpawnRate + MRamp_SpawnRate + LRamp_SpawnRate) && randomValue > (SRamp_SpawnRate + MRamp_SpawnRate)) {
+            poolManager = PoolingSystem.Instance.getPoolManagerInstance(LRampManagerContainer);
+            rampPrefab = poolManager.GetPoolableObject<PoolableObject>();
         }
 
         if (rampPrefab != null) {
-            Instantiate(rampPrefab, SpawnPoint.transform.position, Quaternion.identity);
+            rampPrefab.transform.position = SpawnPoint.transform.position;
         }
     }
 
