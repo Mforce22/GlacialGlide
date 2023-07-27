c7968e942d8f1ff0c58db92b4ca28ac4c6a38c4b
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-07-27 15:34:57 +0200

    Fixed UI option view

--- Assets/Scripts/UI/OptionsViewController.cs
+++ Assets/Scripts/UI/OptionsViewController.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionsViewController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _button;
+    private void Start() {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        if(currentSceneName == "MainMenu") {
+            _button.SetActive(false);
+        }
+    }
     public void ChangeScene(string scene) {
         TravelSystem.Instance.SceneLoad(scene);
         AudioSystem.Instance.StopMusic();
