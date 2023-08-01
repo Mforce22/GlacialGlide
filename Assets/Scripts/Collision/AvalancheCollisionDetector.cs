0166ab95e7f043fe473c1cadc6855bd0778ddaeb
Magurd <matte.guardi@gmail.com>
2023-08-01 12:30:17 +0200

    Added avalanche movement and damage

---
+++ Assets/Scripts/Collision/AvalancheCollisionDetector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheCollisionDetector : MonoBehaviour
+{
+
+    [Header("Events")]
+    [Tooltip("Event to invoke when the object hit the player")]
+    [SerializeField]
+    private GameEvent _EventToInvoke;
+
+
+    [Header("Pool Manager Id")]
+    [Tooltip("Id of the pool manager to use")]
+    [SerializeField]
+    private IdContainer _idPoolManager;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger detected " + gameObject.name);
+        _EventToInvoke.Invoke();
+        _EventToInvoke.Invoke();//avalanche do 2 damage
+        PoolManager poolManager = PoolingSystem.Instance.getPoolManagerInstance(_idPoolManager);
+        poolManager.ReturnPoolableObject(gameObject.GetComponent<PoolableObject>());
+    }
+
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     Debug.Log("Collision detected");
+    // }
+}
---
+++ Assets/Scripts/Movement/AvalancheMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheMovement : MonoBehaviour
+{
+    [Header("Events")]
+    [Tooltip("Event to subscribe to change the speed of the object")]
+    [SerializeField]
+    private GameEvent _ChangeSpeedEvent;
+
+    [Header("Movement")]
+    private int _movement;
+
+    private void Awake()
+    {
+        _ChangeSpeedEvent.Subscribe(SpeedChanged);
+    }
+
+    private void OnDisable()
+    {
+        _ChangeSpeedEvent.Unsubscribe(SpeedChanged);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get instance of GameMaster
+        _movement = GameMaster.Instance.getVelocity();
+
+        //used for testing
+        _ChangeSpeedEvent.Subscribe(SpeedChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the game isn't in pause
+        if (!GameMaster.Instance.getPause())
+        {
+            //move up 
+            transform.Translate(Vector3.down * _movement * Time.deltaTime);
+        }
+    }
+
+    void SpeedChanged(GameEvent evt)
+    {
+        _movement = GameMaster.Instance.getVelocity();
+        Debug.Log("Speed changed to " + _movement);
+    }
+}
