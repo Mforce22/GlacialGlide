efb070d7597a7d7d1b0c2ee11dd8601d5cf55ca4
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-08-09 17:37:50 +0200

    Commented Audio System

--- Assets/Scripts/Core/Audio/AudioSystem.cs
+++ Assets/Scripts/Core/Audio/AudioSystem.cs
@@ -3,37 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// This class manages the audio system in the game, controlling music playback and volume.
+/// It inherits from Singleton<AudioSystem> and implements ISystem.
+/// </summary>
 public class AudioSystem : Singleton<AudioSystem>, ISystem {
-
     [SerializeField]
-    private int _Priority;
-    public int Priority { get => _Priority; }
+    private int _Priority; // Priority of the audio system.
+    public int Priority { get => _Priority; } // Property to access the priority.
 
-    //Music
-    private AudioSource musicSource;
+    private AudioSource musicSource; // AudioSource component for playing music.
 
-
+    /// <summary>
+    /// Sets up the audio system by configuring the AudioSource component.
+    /// </summary>
     public void Setup() {
-        musicSource = gameObject.GetComponent<AudioSource>();
+        musicSource = GetComponent<AudioSource>();
         musicSource.loop = true;
         musicSource.playOnAwake = false;
-        musicSource.volume = 0.5f; // Set volume
+        musicSource.volume = 0.5f; // Default volume.
 
+        // Signal that the system setup is finished.
         SystemCoordinator.Instance.FinishSystemSetup(this);
     }
 
-
+    /// <summary>
+    /// Sets the volume of the music.
+    /// </summary>
+    /// <param name="volume">The desired volume value.</param>
     public void SetVolume(float volume) {
-        //volume = Mathf.Clamp01(volume);
-        //Debug.Log("Volume: " + volume.ToString());
         musicSource.volume = volume;
     }
 
+    /// <summary>
+    /// Starts playing the music.
+    /// </summary>
     public void StartMusic() {
-        // start music
         musicSource.Play();
     }
 
+    /// <summary>
+    /// Stops playing the music.
+    /// </summary>
     public void StopMusic() {
         musicSource.Stop();
     }
