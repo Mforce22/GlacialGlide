80c0f7938857f3bf5a9f9db53dd95a104d876a8b
Matteo Guardini <matte.guardi@gmail.com>
2023-08-29 14:39:55 +0200

    Fixed jump and removed old input

--- Assets/Scripts/UI/JumpViewController.cs
+++ Assets/Scripts/UI/JumpViewController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
 
-public class JumpViewController : MonoBehaviour {
+public class JumpViewController : MonoBehaviour
+{
     #region Events
     [Tooltip("Event to invoke when the jump is completed with success")]
     [SerializeField]
@@ -18,18 +19,29 @@ public class JumpViewController : MonoBehaviour {
     private Vector3 _vect = new Vector3(0.04f, 0.04f, 0);
     #endregion
 
-    private void Start() {
+    private void Start()
+    {
         StartCoroutine(Reduce());
     }
 
-    private IEnumerator Reduce() {
-        while (_RedCircle.transform.localScale.x >= 0.5f) {
+    private IEnumerator Reduce()
+    {
+        bool _canEasyJump = GameMaster.Instance.getEasyJump();
+        bool _canMediumJump = GameMaster.Instance.getMediumJump();
+        bool _canHardJump = GameMaster.Instance.getHardJump();
+        while (_RedCircle.transform.localScale.x >= 0.5f)
+        {
             _RedCircle.transform.localScale = _RedCircle.transform.localScale - _vect;
-            if (GameMaster.Instance.getEasyJump()) {
+            if (_canEasyJump)
+            {
                 yield return new WaitForSeconds(0.1f);
-            } else if (GameMaster.Instance.getMediumJump()) {
+            }
+            else if (_canMediumJump)
+            {
                 yield return new WaitForSeconds(0.08f);
-            } else if (GameMaster.Instance.getHardJump()) {
+            }
+            else if (_canHardJump)
+            {
                 yield return new WaitForSeconds(0.06f);
             }
         }
@@ -39,12 +51,16 @@ public class JumpViewController : MonoBehaviour {
     }
 
     [ContextMenu("Click debug")]
-    public void Click() {
-        if (_RedCircle.transform.localScale.x >= 0.6f && _RedCircle.transform.localScale.x <= 1.1f) {
+    public void Click()
+    {
+        if (_RedCircle.transform.localScale.x >= 0.6f && _RedCircle.transform.localScale.x <= 1.1f)
+        {
             Debug.Log("JUMP COMPLETED");
             _JumpCompleted.Invoke();
             Destroy(gameObject);
-        } else {
+        }
+        else
+        {
             Debug.Log("JUMP FAILED");
             _JumpFailed.Invoke();
             Destroy(gameObject);
