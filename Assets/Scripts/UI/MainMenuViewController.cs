abb7e9d280acb3ced6b54eef229581c5872cd7df
Matteo Guardini <matte.guardi@gmail.com>
2023-08-30 18:28:03 +0200

    from mainmenu to pg1 using state machine

--- Assets/Scripts/UI/MainMenuViewController.cs
+++ Assets/Scripts/UI/MainMenuViewController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 /// <summary>
 /// This class manages the main menu UI and provides methods to navigate to different scenes and open options and credits views.
 /// </summary>
-public class MainMenuViewController : MonoBehaviour {
+public class MainMenuViewController : MonoBehaviour
+{
     [SerializeField]
     private OptionsViewController _OptionsViewPrefab; // Prefab for the options view.
     [SerializeField]
@@ -19,15 +20,18 @@ public class MainMenuViewController : MonoBehaviour {
     /// This method is called to change the game scene when a menu option is selected.
     /// </summary>
     /// <param name="scene">The name of the scene to transition to.</param>
-    public void ChangeScene(string scene) {
+    public void ChangeScene(string scene)
+    {
         // Load the specified scene.
-        TravelSystem.Instance.SceneLoad(scene);
+        FlowSystem.Instance.TriggerFSMEvent(scene);
+        //TravelSystem.Instance.SceneLoad(scene);
     }
 
     /// <summary>
     /// This method is called to open the options view.
     /// </summary>
-    public void OpenOptions() {
+    public void OpenOptions()
+    {
         // Instantiate the options view prefab if not already instantiated.
         if (_optionsViewController) return;
         _optionsViewController = Instantiate(_OptionsViewPrefab);
@@ -36,13 +40,15 @@ public class MainMenuViewController : MonoBehaviour {
     /// <summary>
     /// This method is called to open the credits view.
     /// </summary>
-    public void OpenCredits() {
+    public void OpenCredits()
+    {
         // Instantiate the credits view prefab if not already instantiated.
         if (_creditsViewController) return;
         _creditsViewController = Instantiate(_CreditsViewPrefab);
     }
 
-    public void OpenHowToPlay() {
+    public void OpenHowToPlay()
+    {
         if (_HowToPlayViewController) return;
         _HowToPlayViewController = Instantiate(_HowToPlayViewPrefab);
     }
