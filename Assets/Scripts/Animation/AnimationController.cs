bd1b09226c658b0796590a3b062c27ef79dcf878
Matteo Guardini <matte.guardi@gmail.com>
2023-08-30 13:48:13 +0200

    Added animation for big jump

--- Assets/Scripts/Animation/AnimationController.cs
+++ Assets/Scripts/Animation/AnimationController.cs
@@ -24,6 +24,16 @@ public class AnimationController : MonoBehaviour
     [SerializeField]
     private GameEvent _bombEvent;
 
+
+    //create an enum for the animation states
+    private enum AnimationState
+    {
+        None,
+        Jump,
+        BigJump
+    }
+
+    private AnimationState _currentAnimationState = AnimationState.None;
     private void OnEnable()
     {
         _jumpStartEvent.Subscribe(OnJumpStart);
@@ -44,12 +54,29 @@ public class AnimationController : MonoBehaviour
 
     private void OnJumpStart(GameEvent evt)
     {
-        _animator.SetTrigger("StartJump");
+        if (GameMaster.Instance.getHardJump())
+        {
+            _animator.SetTrigger("StartBigJump");
+            _currentAnimationState = AnimationState.BigJump;
+        }
+        else
+        {
+            _animator.SetTrigger("StartJump");
+            _currentAnimationState = AnimationState.Jump;
+        }
     }
 
     private void OnJumpEnd(GameEvent evt)
     {
-        _animator.SetTrigger("JumpEnded");
+        if (_currentAnimationState == AnimationState.BigJump)
+        {
+            _animator.SetTrigger("EndBigJump");
+        }
+        else
+        {
+            _animator.SetTrigger("JumpEnded");
+        }
+        _currentAnimationState = AnimationState.None;
     }
 
     private void OnDamageTaken(GameEvent evt)
