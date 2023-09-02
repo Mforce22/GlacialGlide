a9765fc3f919f2265f413b5eadeacaef144619ac
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-09-02 11:15:19 +0200

    Commented movement and shield scripts

--- Assets/Scripts/Shield/ShieldManager.cs
+++ Assets/Scripts/Shield/ShieldManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// Manages the behavior of the shield power-up in the game.
+/// </summary>
 public class ShieldManager : MonoBehaviour {
     [Tooltip("The shield prefab")]
     [SerializeField]
@@ -13,22 +16,37 @@ public class ShieldManager : MonoBehaviour {
     [SerializeField]
     private GameEvent _onShieldRemoved;
 
-
+    /// <summary>
+    /// Subscribes to events when the shield manager is enabled.
+    /// </summary>
     private void OnEnable() {
         _onShieldTaken.Subscribe(OnShieldTaken);
         _onShieldRemoved.Subscribe(OnShieldRemoved);
     }
 
+    /// <summary>
+    /// Unsubscribes from events when the shield manager is disabled.
+    /// </summary>
     private void OnDisable() {
         _onShieldTaken.Unsubscribe(OnShieldTaken);
         _onShieldRemoved.Unsubscribe(OnShieldRemoved);
     }
 
+    /// <summary>
+    /// Activates the shield when it is taken.
+    /// </summary>
+    /// <param name="evt">The event associated with the shield being taken.</param>
     private void OnShieldTaken(GameEvent evt) {
+        // Activate the shield GameObject.
         _shield.SetActive(true);
     }
 
+    /// <summary>
+    /// Deactivates the shield when it is removed or expires.
+    /// </summary>
+    /// <param name="evt">The event associated with the shield being removed or expiring.</param>
     private void OnShieldRemoved(GameEvent evt) {
+        // Deactivate the shield GameObject.
         _shield.SetActive(false);
     }
 
